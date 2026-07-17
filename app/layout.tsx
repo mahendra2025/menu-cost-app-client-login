050c36085b90773feeae69be06979ff4148cac04
mahendra2025 <166189480+mahendra2025@users.noreply.github.com>
2026-07-17 09:13:15 +0530

    Add data-theme attribute to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export const viewport: Viewport = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+  <html lang="en" data-theme="dark">
       <body>
         {children}
         <PwaRegister />
